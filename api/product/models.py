ca0dc01f63c096611d438439c8ad9bd7cf157492
vanmynhanvi <vanmynhanvi@gmail.com>
2024-11-11 11:32:10 +0700

    product management

--- api/product/models.py
+++ api/product/models.py
@@ -1,13 +1,98 @@
 from django.db import models
 import uuid
 from django.core.validators import MinValueValidator,MaxLengthValidator
+product_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+product_details_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+color_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+product_colors_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+size_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+product_sizes_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+material_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+product_materials_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+type_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+product_types_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+material_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
+product_materials_config = {
+    "status": {
+        'ACTIVE': 'active', 
+        'IN_ACTIVE': 'in_active', 
+        'DELETED': 'deleted'
+    }
+}
 # Product model
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=250)
-    price = models.CharField(max_length=50)
-    status = models.BooleanField(default=True, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    description = models.TextField(default='')
+    price = models.FloatField(default= 0)
+    status = models.CharField(max_length=10, default= product_config.get('status').get('ACTIVE'))
+    image = models.CharField(max_length=250, default = '')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # relation
@@ -19,7 +104,7 @@ class Product(models.Model):
 class Color(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=50)
-    status = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, default= color_config.get('status').get('ACTIVE'))
 
     def __str__(self):
         return self.name
@@ -28,7 +113,7 @@ class Color(models.Model):
 class Size(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=50)
-    status = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, default= size_config.get('status').get('ACTIVE'))
     # relation
     def __str__(self):
         return self.name
@@ -37,19 +122,64 @@ class Size(models.Model):
 class ProductDetails(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     qty = models.IntegerField(default=0)
-    status = models.BooleanField(default=True, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    status = models.CharField(max_length=10, default= product_details_config.get('status').get('ACTIVE'))
+    image = models.CharField(max_length=250, default = '')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # relation
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='details')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     def __str__(self):
         return f'{self.product.name} - {self.color.name} - {self.size.name}'
-    
     class Meta:
         unique_together = ('product', 'size', 'color')
+
+# Type model
+class Type(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=10, default= type_config.get('status').get('ACTIVE'))
+    # relation
+    def __str__(self):
+        return self.name
+    
+# ProductTypes model
+class ProductTypes(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    status = models.CharField(max_length=10, default= product_types_config.get('status').get('ACTIVE'))
+    # relation
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.type.name}'
+    
+    class Meta:
+        unique_together = ('product', 'type')
+    
+# Material model
+class Material(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=10, default= material_config.get('status').get('ACTIVE'))
+    # relation
+    def __str__(self):
+        return self.name
+    
+# product materials model
+class ProductMaterials(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    status = models.CharField(max_length=10, default= product_materials_config.get('status').get('ACTIVE'))
+    # relation
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.material.name}'
+    
+    class Meta:
+        unique_together = ('product', 'material')
 
 class feedback(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
